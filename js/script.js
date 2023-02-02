e47a086217449b95fbca5f8ab08487f7386dea47
teresala14 <teresala.45@gmail.com>
2023-02-01 19:07:50 -0500

    added/js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 console.log("Hello! Welcome to My Portfolio!")
 
+function menuToggle() { 
+    var x= document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle'){
+    x.className += ' responsive';}
+    else {
+        x.className = 'navtoggle';
+      }
+}
